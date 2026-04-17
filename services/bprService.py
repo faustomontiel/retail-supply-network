528e6daac9bc5ddb5c4cec3d0506af8ab96fcac1
faustomontiel <faustomontiel98@outlook.com>
2026-04-17 20:25:19 -0300

    bpr service

---
+++ services/bprService.py
@@ -0,0 +1,9 @@
+from services.messageService import messageService, message
+
+class bprService(messageService):
+    def __init__():
+        super().__init__()
+
+class bprMessage(message):
+    def __init__():
+        super().__init__()
